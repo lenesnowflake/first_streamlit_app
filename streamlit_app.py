aa4329ccec65e5a1e8a4813a291144d2edf739cd
Lene Hansen <110986552+lenesnowflake@users.noreply.github.com>
2022-08-15 14:13:25 +0200

    Update streamlit_app.py
    Stop streamlit while troubleshooting

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,6 +33,9 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
 
+#don't run anything past here while we troubleshoot 
+streamlit.stop()
+
 #Querying the trial account metadata 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
