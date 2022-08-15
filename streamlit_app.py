dd11d5a0ea17bd24232a4a75f0b0031de2ab876d
Lene Hansen <110986552+lenesnowflake@users.noreply.github.com>
2022-08-15 10:05:03 +0200

    Update streamlit_app.py
    Querying the trial account metadata

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,3 +31,12 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_c
 #display the same api response with key value pairs displayed in a table structure
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
+
+
+#Querying the trial account metadata 
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
